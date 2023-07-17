886daecf7a5f6801362a6b4e4d7e23bae9963cb3
Tshiamomaema <tshiamomaema9@gmail.com>
2023-07-17 10:00:37 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * maain - starting point
+ * main - Starting point
  *
- * Return: only 0
+ * Return: Always 0
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
