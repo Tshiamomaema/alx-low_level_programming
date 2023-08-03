2f159c7bc8f7450759cd38b65ca2e21be6094e10
Tshiamomaema <tshiamomaema9@gmail.com>
2023-08-03 03:32:47 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Takes in a string and prints it.
+ *@s:  the string that will be added
+ *
+ * return - nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar("%c", *s);
+}
